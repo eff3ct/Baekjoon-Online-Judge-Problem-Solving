ffce2d2a5ac2a53a44c7eb8e5c4de84bf92d7502
2021093054 <effect2110@gmail.com>
2021-08-10 15:03:13 +0900

    BOJ 15686 // 치킨 배달
    <<Brute Force>>

---
+++ 15686/15686.cpp
@@ -0,0 +1,65 @@
+// https://www.acmicpc.net/problem/15686 //
+#include <iostream>
+#include <vector>
+#include <cmath>
+#define INF 987654321
+
+using namespace std;
+
+int ret = INF;
+
+void solve(int idx, vector<pair<int, int>>& chosenChicken, vector<pair<int, int>>& chicken, vector<pair<int, int>>& house, int& M) {
+    static bool visit[13] = { false, };
+
+    if(chosenChicken.size() == M) {
+        int chickenDist = 0;
+        for(auto& element : house) {
+            int hy = element.first;
+            int hx = element.second;
+
+            int minimum = abs(hx - chosenChicken[0].second) + abs(hy - chosenChicken[0].first);
+            for(auto& c : chosenChicken) {
+                int cy = c.first;
+                int cx = c.second;
+
+                minimum = min(minimum, abs(hx - cx) + abs(hy - cy));
+            }
+
+            chickenDist += minimum;
+        }
+        ret = min(ret, chickenDist);
+        return;
+    }
+
+    for(int i = idx; i < chicken.size(); ++i) {
+        if(visit[i]) continue;
+        visit[i] = true;
+        chosenChicken.push_back(chicken[i]);
+        solve(i + 1, chosenChicken, chicken, house, M);
+        chosenChicken.pop_back();
+        visit[i] = false;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M; cin >> N >> M;
+    vector<pair<int, int>> house;
+    vector<pair<int, int>> chicken;
+    for(int i = 1; i <= N; ++i) {
+        for(int j = 1; j <= N; ++j) {
+            int x; cin >> x;
+            if(x == 1) house.push_back({i, j});
+            else if(x == 2) chicken.push_back({i, j});
+        }
+    }
+
+    vector<pair<int, int>> chosenChicken;
+    solve(0, chosenChicken, chicken, house, M);
+
+    cout << ret;
+    return 0;
+}
